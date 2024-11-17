a08918148c47d7140e1aa890c394a7d00ef87ab7
flurrybun <parkercasey007@gmail.com>
2024-11-17 13:14:48 -0500

    fix secretrewardslayer bg not being darkened

--- src/hooks/MenuBackground.cpp
+++ src/hooks/MenuBackground.cpp
@@ -92,7 +92,7 @@ class $modify(LevelSelectLayer) {
     $override
     ccColor3B colorForPage(int page) {
         if (!night::isNightMode()) return LevelSelectLayer::colorForPage(page);
-        
+
         auto color = LevelSelectLayer::colorForPage(page % 9);
         if (page == 22) color = ccc3(88, 104, 113);
 
@@ -109,12 +109,16 @@ class $modify(LevelSelectLayer) {
 class $modify(SecretRewardsLayer) {
     // getPageColor is inlined, so we have to do things a little differently
     void adjustBackgroundColor() {
-        auto hsv = color::rgb2hsv(m_backgroundSprite->getColor());
+        // SecretRewardsLayer members are a bit messed up atm so we can't use m_backgroundSprite
+        auto bg = typeinfo_cast<CCSprite*>(getChildByID("background"));
+        if (!bg) return;
+
+        auto hsv = color::rgb2hsv(bg->getColor());
 
         hsv.s *= 0.85f;
         hsv.v *= 0.4f;
 
-        m_backgroundSprite->setColor(night::adjustBrightness(color::hsv2rgb(hsv)));
+        bg->setColor(night::adjustBrightness(color::hsv2rgb(hsv)));
     }
 
     $override
